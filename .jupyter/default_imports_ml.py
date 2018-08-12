a1893785b996f88b19184d9a6761c97553062a28
mazzma12 <matthieu.mazzolini@gmail.com>
2018-08-12 22:13:19 +0200

    PYTHON: more imports

--- .jupyter/default_imports_ml.py
+++ .jupyter/default_imports_ml.py
@@ -2,55 +2,59 @@ import cufflinks
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import (
-        TruncatedSVD,
-        PCA,
-        IncrementalPCA
-        )
+    TruncatedSVD,
+    PCA,
+    IncrementalPCA
+)
 from sklearn.ensemble import (
-        AdaBoostClassifier,
-        GradientBoostingRegressor,
-        RandomForestClassifier,
-        )
+    AdaBoostClassifier,
+    GradientBoostingRegressor,
+    RandomForestClassifier,
+)
+from sklearn.feature_extraction.text import (
+    CountVectorizer,
+    TfidfVectorizer,
+)
 from sklearn.feature_selection import (
-        RFECV,
-        )
+    RFECV,
+)
 from sklearn.linear_model import (
-        ElasticNet,
-        ElasticNetCV,
-        SGDClassifier,
-        SGDRegressor,
-        Lasso,
-        LassoCV,
-        LinearRegression,
-        LogisticRegression,
-        LogisticRegressionCV,
-        HuberRegressor,
-        Ridge,
-        RidgeCV,
-        )
+    ElasticNet,
+    ElasticNetCV,
+    SGDClassifier,
+    SGDRegressor,
+    Lasso,
+    LassoCV,
+    LinearRegression,
+    LogisticRegression,
+    LogisticRegressionCV,
+    HuberRegressor,
+    Ridge,
+    RidgeCV,
+)
 from sklearn.metrics import (
-        accuracy_score,
-        confusion_matrix,
-        homogeneity_score,
-        mean_absolute_error,
-        mean_squared_error,
-        roc_auc_score,
-        )
+    accuracy_score,
+    confusion_matrix,
+    homogeneity_score,
+    mean_absolute_error,
+    mean_squared_error,
+    roc_auc_score,
+)
 from sklearn.model_selection import (
-        TimeSeriesSplit,
-        StratifiedKFold,
-        train_test_split,
-        GridSearchCV
+    TimeSeriesSplit,
+    StratifiedKFold,
+    train_test_split,
+    GridSearchCV
 )
 from sklearn.pipeline import (
-        make_pipeline,
-        Pipeline
-        )
+    make_pipeline,
+    Pipeline
+)
 from sklearn.preprocessing import (
-        FunctionTransformer,
-        PolynomialFeatures,
-        StandardScaler,
-        MinMaxScaler,
-        LabelEncoder,
-        )
+    FunctionTransformer,
+    PolynomialFeatures,
+    StandardScaler,
+    MinMaxScaler,
+    LabelEncoder,
+)
 from tqdm import tqdm
