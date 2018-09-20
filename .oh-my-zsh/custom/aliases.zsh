df0fb2a26be76468f52586ed824f13868c640434
mazzma12 <matthieu.mazzolini@gmail.com>
2018-09-21 00:00:31 +0200

    Alias env edit

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -7,8 +7,10 @@
 alias zshconfig="$EDITOR ~/.zshrc"
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias ae="$EDITOR $ZSH_CUSTOM/aliases.zsh"
+alias enve="$EDITOR $ZSH_CUSTOM/env.zsh"
 alias ve="$EDITOR $HOME/.vimrc"
 alias nve="$EDITOR $HOME/.config/nvim/init.vim"
+
 
 # System
 alias rm=trash
