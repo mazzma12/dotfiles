49964012300ceb613fe4a00f62aa5efd7ef64f39
Matthieu Mazzolini <m.mazzolini@kayrros.com>
2018-10-12 08:20:16 +0200

    [I3] Add wallpaper crawling to startup

--- .config/i3/bg.sh
+++ .config/i3/bg.sh
@@ -6,9 +6,10 @@ n=${1:-3}
 mkdir -p $bg_folder
 for i in $(seq 0 $n)
 do
-	echo $i
 	wget -q -O $bg_folder/$RANDOM.jpeg https://source.unsplash.com/random/$bg_dim
+	sleep .1; # True Random
 done
 fdupes -dN $bg_folder
+# Display bg with :
 # feh --randomize --bg-fill $bg_folder
 
