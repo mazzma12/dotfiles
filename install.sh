6c4c301d17791d5188f788834dc5b807155eb8ac
mazzma12 <matthieu.mazzolini@gmail.com>
2019-01-26 18:11:25 +0100

    Refactor install script with branch install

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 main(){
 	set -e;
 	export DOTFILES_HOME="$HOME/.cfg"
+	export branch=${1:-master}
+	echo "Cloning from branch: $branch"
 
 	if [ -d $DOTFILES_HOME ]; then
 		printf "Dotfiles already installed.\n"
@@ -17,10 +19,20 @@ main(){
 		mv $HOME/.profile $HOME/.profile.bak
 	fi
 
-	git clone --bare https://github.com/mazzma12/dotfiles.git $DOTFILES_HOME && \
-	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME checkout -- && \
-	/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME config --local status.showUntrackedFiles no;
+	git clone --bare https://github.com/mazzma12/dotfiles.git --branch $branch $DOTFILES_HOME 
 
+	function config {
+		/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+	}
+	config checkout 
+	if [ $? = 0 ]; then
+		echo "Checked out config.";
+	else
+		echo "Backing up pre-existing dot files to with .bak suffixes";
+		config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} {}.bak
+	fi;
+	config checkout
+	config config status.showUntrackedFiles no
 
 	if which nvim >/dev/null 2>&1; then
 		nvim +slient +VimEnter +PlugInstall +qall > /dev/null 
