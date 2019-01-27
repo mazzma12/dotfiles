4b61ed2c14ccf1595c48e712b13397c1856d2f95
mazzma12 <matthieu.mazzolini@gmail.com>
2019-01-27 22:47:14 +0100

    Prompte before o erwritting in install

--- install.sh
+++ install.sh
@@ -9,16 +9,6 @@ main(){
 		printf "Dotfiles already installed.\n"
 	fi
 	
-	if [ -f $HOME/.zshrc ]; then
-		printf "Moving old zsh config to .zshrc.bak.\n"
-		mv $HOME/.zshrc $HOME/.zshrc.bak
-	fi
-
-	if [ -f $HOME/.profile ]; then
-		printf "Moving old profile config to .profile.bak\n"
-		mv $HOME/.profile $HOME/.profile.bak
-	fi
-
 	git clone --bare https://github.com/mazzma12/dotfiles.git --branch $branch $DOTFILES_HOME 
 
 	function config {
@@ -28,7 +18,7 @@ main(){
 		echo "Checked out config.";
 	else
 		echo "Backing up pre-existing dot files to with .bak suffixes";
-		config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} {}.bak
+		config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv -i {} {}.bak
 	fi;
 	config checkout
 	config config status.showUntrackedFiles no
