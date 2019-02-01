1e80c9f97fb3410f858e198dcae826599bca3981
mazzma12 <matthieu.mazzolini@gmail.com>
2019-02-01 08:59:58 +0100

    [i3] Fix background scroller

--- .config/i3/crawl_bg.sh
+++ .config/i3/crawl_bg.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 bg_dim="2560x1440"
 bg_folder="$HOME/Pictures/wallpapers/$bg_dim"
 mkdir -p $bg_folder
-while [ $(ls -l $HOME/Pictures/wallpapers | wc -l) -lt $1 ]; do
+while [ $(ls -l $bg_folder | wc -l) -lt $1 ]; do
 	wget -q -O $bg_folder/$RANDOM.jpeg https://source.unsplash.com/random/$bg_dim
 done
 fdupes -dN $bg_folder
