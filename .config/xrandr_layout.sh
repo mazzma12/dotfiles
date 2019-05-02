362d1e0e927f7402ad3a25c48cd2300bfdd55465
Matthieu Mazzolini <m.mazzolini@kayrros.com>
2019-05-02 11:07:11 +0200

    Fix new xrandr display names with Ubuntu 18.04

--- .config/xrandr_layout.sh
+++ .config/xrandr_layout.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-monitor_left="DP1"
-monitor_center="DP2"
-monitor_right="eDP1"
+monitor_left="DP-1"
+monitor_center="DP-2"
+monitor_right="eDP-1"
 
-xrandr --output VIRTUAL1 --off --output "$monitor_left" --mode 1920x1080 --pos 0x0 --rotate normal --output "$monitor_right" --primary --mode 1920x1080 --scale 1x1 --pos 3840x0 --rotate normal --output "$monitor_center" --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output "$monitor_left" --mode 1920x1080 --pos 0x0 --rotate normal --output "$monitor_right" --primary --mode 1920x1080 --scale 1x1 --pos 3840x0 --rotate normal --output "$monitor_center" --mode 1920x1080 --pos 1920x0 --rotate normal
