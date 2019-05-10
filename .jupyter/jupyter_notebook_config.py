30f689f94b6203c1392ba0504239a5a388e5066f
mazzma12 <matthieu.mazzolini@gmail.com>
2019-05-10 11:21:30 +0200

    [JUPYTER] Add julia build config

--- .jupyter/jupyter_notebook_config.py
+++ .jupyter/jupyter_notebook_config.py
@@ -693,3 +693,15 @@ c.NotebookApp.tornado_settings = {'max_body_size': 104857600, 'max_buffer_size':
 #
 #  By default, all installed kernels are allowed.
 #c.KernelSpecManager.whitelist = set()
+
+###JULIA-WEBIO-CONFIG-BEGIN
+import sys, os
+if os.path.isfile("/home/mazzo/.julia/packages/WebIO/Rk8wc/deps/jlstaticserve.py"):
+    sys.path.append("/home/mazzo/.julia/packages/WebIO/Rk8wc/deps")
+    c = get_config()
+    c.NotebookApp.nbserver_extensions = {
+        "jlstaticserve": True
+    }
+else:
+    print("WebIO config in ~/.jupyter/jupyter_notebook_config.py but WebIO plugin not found")
+###JULIA-WEBIO-CONFIG-END
