af35d216b72f7214782a7c3e7cc9c6d635fe12ef
mazzma12 <matthieu.mazzolini@gmail.com>
2019-09-14 10:27:46 +0200

    Update python oneliner

--- .jupyter/oneliners.py
+++ .jupyter/oneliners.py
@@ -5,20 +5,27 @@ import logging
 
 
 def debug_packages(*packages):
-    (logging.getLogger(pp).setLevel('DEBUG') for pp in packages)
+    (logging.getLogger(pp).setLevel("DEBUG") for pp in packages)
 
 
 def _join(*items):
-    return '_'.join(map(str, items))
+    return "_".join(map(str, items))
 
 
 def sort_dict(d, reverse=False):
-    return OrderedDict((sorted_key, d[sorted_key]) for sorted_key in sorted(d, key=d.get, reverse=reverse))
+    return OrderedDict(
+        (sorted_key, d[sorted_key])
+        for sorted_key in sorted(d, key=d.get, reverse=reverse)
+    )
 
 
-def _join_suffix(*items, suffix=('x', 'y'), sep='_'):
+def _join_suffix(*items, suffix=("x", "y"), sep="_"):
     return [sep.join([it, ss]) for it in items for ss in suffix]
 
 
 def format_string_with_utc(s):
-    return '_'.join([datetime.datetime.utcnow().strftime("%Y%m%d%H%M%S"), s])
+    return "_".join([datetime.datetime.utcnow().strftime("%Y%m%d%H%M%S"), s])
+
+
+def chunk(list, size):
+    return [list[i : i + size] for i in range(0, len(list), size)]
