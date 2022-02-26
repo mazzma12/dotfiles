2276f661d3ac10b89740687f5328bf78934b64a5
mazzma12 <mazzma12@protonmail.com>
2022-02-26 16:20:11 +0100

    Fix crawl unsplash script

--- .config/i3/crawl_bg.sh
+++ .config/i3/crawl_bg.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Enter a number X
+# Download X images at random from unsplash, and delete duplicates in the end
+n_images=${1:-3}
 bg_dim="2560x1440"
 bg_folder="$HOME/Pictures/wallpapers/$bg_dim"
 mkdir -p $bg_folder
-while [ $(ls -l $HOME/Pictures/wallpapers | wc -l) -lt $1 ]; do
+while [ $(ls -l $bg_folder | wc -l) -lt $n_images ]; do
 	wget -q -O $bg_folder/$RANDOM.jpeg https://source.unsplash.com/random/$bg_dim
 done
 fdupes -dN $bg_folder
